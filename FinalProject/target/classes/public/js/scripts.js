812582f629c05146906d41470cd1c60f0831523b
sdeaugustine33 <sqd5358@psu.edu>
2018-03-29 14:14:29 -0400

    V.01

--- FinalProject/target/classes/public/js/scripts.js
+++ FinalProject/target/classes/public/js/scripts.js
@@ -2,7 +2,5 @@ $(function() {
     $('#messages li').click(function() {
         $(this).fadeOut();
     });
-    setTimeout(function() {
-        $('#messages li.info').fadeOut();
-    }, 3000);
+    
 });
